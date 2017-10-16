9df3588a7791651df61cf177d6f90fa974459914
Marlos Pomin <marlospomin@gmail.com>
2017-10-16 21:02:13 -0200

    Add comments

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,18 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync');
 
+// Live reload task
 gulp.task('sync', function () {
   browserSync.init({
     server: "./"
   });
 
+  // Watch for file changes
   gulp.watch(['./scss/**/**'], ['sass']);
   gulp.watch(["./*.html", "./js/*.js"], browserSync.reload);
 });
 
+// Compile sass files
 gulp.task('sass', function () {
   return gulp.src('./scss/main.scss')
     .pipe(sass().on('error', sass.logError))
@@ -18,4 +21,5 @@ gulp.task('sass', function () {
     .pipe(browserSync.stream());
 });
 
+// Set the default task as 'sync'
 gulp.task('default', ['sync']);
