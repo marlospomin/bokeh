f10d085b00fed00c6613d892ec5c7ad37417607b
Marlos Pomin <marlospomin@gmail.com>
2017-11-12 22:05:18 -0200

    Add navbar animation

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-// Your code goes here
+// Get the navbar
+const navbar = document.querySelector('header');
+// Shrink when the user scrolled 100px
+const shrinkOn = 100;
+
+window.addEventListener('scroll', () => {
+  // Track scroll position
+  let distanceY = window.scrollY;
+
+  window.requestAnimationFrame(() => {
+    // If the user scrolled more than the treshold, add class
+    if (distanceY > shrinkOn) {
+      navbar.classList.add('shrink');
+    } else {
+      navbar.classList.remove('shrink');
+    }
+  });
+});
